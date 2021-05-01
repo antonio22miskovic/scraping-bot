d8f692728d79c15e42a7ca60a9ce623f87789ba7
antonio22miskovic <villalobosantonio79@gmail.com>
2021-04-30 20:02:38 -0400

    subiendo el bot con cron

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cron = require('node-cron')
 client.on('ready', () => {
  	console.log('conectado a discord')
  })//1
-// client.login('ODM3NzgxOTg1OTgzNTk0NTQ2.YIxjRg.wGP9iIDgE7NUhQRnzFPokzVm4SU')
-client.login('ODM2MjQ1NzI4Mjk2ODI4OTkw.YIbMhg.3cerBRNlet7JjN70FI_iVwxAbWs') //mio
+client.login('ODM3NzgxOTg1OTgzNTk0NTQ2.YIxjRg.wGP9iIDgE7NUhQRnzFPokzVm4SU')
+// client.login('ODM2MjQ1NzI4Mjk2ODI4OTkw.YIbMhg.3cerBRNlet7JjN70FI_iVwxAbWs') //mio
 
 cron.schedule('15 * * * * *',() => {
 	  initTaf()
